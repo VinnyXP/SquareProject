4e652fbfc0fb423d6061d09696a63d2e7927724f
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-04-07 21:47:38 -0500

    Add login component

--- order-template-react-original/src/App.jsx
+++ order-template-react-original/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import OrderForm from './components/OrderForm'
+import LoginPage from './components/LoginPage'
 
 function App() {
 
   return (
     <>
+      <LoginPage />
       <OrderForm />
       <div className="card">
 
