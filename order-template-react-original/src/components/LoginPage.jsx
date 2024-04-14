137e8bb5397b8b4ff39a355134ab78414fcb4cb5
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-04-14 18:25:40 -0500

    Set up the start of routing

--- order-template-react-original/src/components/LoginPage.jsx
+++ order-template-react-original/src/components/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Item from './Item';
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
@@ -15,33 +16,39 @@ export default function LoginPage() {
     }
 
     return (
-        <form onSubmit={handleLoginSubmit}>
-            <div >
-                <div>Login</div>
+        <>
+            <form onSubmit={handleLoginSubmit}>
+                <div >
+                    <div>Login</div>
+                </div>
+                <br />
+                <div className="login-input-div">
+                    <label>Email</label>
+                    <input
+                        value={email}
+                        onChange={(ev) => setEmail(ev.target.value)}
+                        className={'inputBox'}
+                    />
+                    <label className="errorLabel">{emailError}</label>
+                </div>
+                <br />
+                <div className="login-input-div">
+                    <label>Password</label>
+                    <input
+                        value={password}
+                        onChange={(ev) => setPassword(ev.target.value)}
+                        className={'inputBox'}
+                    />
+                    <label className="errorLabel">{passwordError}</label>
+                </div>
+                <br />
+                <button className={'inputButton'} type="submit" value={'Log in'} >Log In</button>
+            </form>
+            <div>
+                <p>Dont have an account?</p>
+                <Link to="/CreateAccount">Create Account</Link>
             </div>
-            <br />
-            <div className="login-input-div">
-                <label>Email</label>
-                <input
-                    value={email}
-                    onChange={(ev) => setEmail(ev.target.value)}
-                    className={'inputBox'}
-                />
-                <label className="errorLabel">{emailError}</label>
-            </div>
-            <br />
-            <div className="login-input-div">
-                <label>Password</label>
-                <input
-                    value={password}
-                    onChange={(ev) => setPassword(ev.target.value)}
-                    className={'inputBox'}
-                />
-                <label className="errorLabel">{passwordError}</label>
-            </div>
-            <br />
-            <button className={'inputButton'} type="submit" value={'Log in'} >Log In</button>
-        </form>
+        </>
     )
 
 
