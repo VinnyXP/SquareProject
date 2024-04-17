16429af8e3f9cf46949a69772cc3f08420ae710f
jech1 <jacob.echeverry13@gmail.com>
2024-04-16 22:41:06 -0400

    Updated Inventory page and Product Info

---
+++ order-template-react-original/src/components/Inventorypage.jsx
@@ -0,0 +1,31 @@
+// contain info about product info componets  
+// product name, inventory, quantity 
+
+class InventoryPage extends React.Component {
+// props contains any data passed to it from a parent component    
+// ?can we just use item instead of product? since item is already useed in the Item component?
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // will set the template for what will be displayed on inventory page
+            // will pass through from product and quantity
+            // Can we get this from the API instead of hardcoding it?
+            
+            // Products = API/DataBase call
+        };
+    }
+
+    render() {
+        return (
+            <div>
+            /* API call to get the inventory */
+                {this.state.products.map((product, index) => (
+                    <ProductInfo key={index} product={product.name} quantity={product.quantity} />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default InventoryPage;
