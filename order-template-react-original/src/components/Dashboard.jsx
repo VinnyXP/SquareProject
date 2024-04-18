48d72422b706850f5229f7df775fb292dd204c38
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-04-17 22:39:31 -0500

    Add pages with orders of certain status

--- order-template-react-original/src/components/Dashboard.jsx
+++ order-template-react-original/src/components/Dashboard.jsx
@@ -6,8 +6,8 @@ export default function Dashboard() {
 
     return (
         <>
-            <Link>Orders to Be Packed</Link>
-            <Link>Orders to Be Delivered</Link>
+            <Link to="/OrdersToBePacked">Orders to Be Packed</Link>
+            <Link to="/OrdersToBeDelivered">Orders to Be Delivered</Link>
             <Link>Reports</Link>
             <Link to="/InventoryPage">Inventory in Hand</Link>
             <Link to="/InventoryPage">Inventory Log</Link>
